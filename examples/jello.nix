6876451042a1fe1628e2d8d240c6deae24a6c19c
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-19 18:18:37 -0500

    Revert "Intermediate State with buildEnv: jello installs hello, but jello not importable"
    This reverts commit 11d29d309eec8dcd8381f2bd28434b6341a9fe6b.

--- examples/jello.nix
+++ examples/jello.nix
@@ -1,21 +1,21 @@
-{ lib, python313Packages, fetchFromGitHub, buildEnv, callPackage, hello, ... }:
+{ lib , buildPythonPackage , fetchFromGitHub , pip , setuptools , hello ,  ... }:
 
-let
-  jello_lib = python313Packages.buildPythonApplication rec {
-    pname = "jello";
-    version = "1.0";
-    pyproject = true;
-    src = fetchFromGitHub {
-      owner = "notarealdeveloper";
-      repo = "jello";
-      rev = "18d6bc75b6d16133ca8b5ebd3db486da36cbecc9";
-      sha256 = "sha256-rnNoGpyab8bbaRA/QuZRIOolv+FTrCGf++IBayVQUMM=";
-    };
-    nativeBuildInputs = with python313Packages; [ pip setuptools ];
-    propagatedBuildInputs = [ hello ];
+buildPythonPackage rec {
+  pname = "jello";
+  version = "1.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "notarealdeveloper";
+    repo = "jello";
+    rev = "18d6bc75b6d16133ca8b5ebd3db486da36cbecc9";
+    sha256 = "sha256-rnNoGpyab8bbaRA/QuZRIOolv+FTrCGf++IBayVQUMM=";
   };
-in
-buildEnv {
-  name = "jello";
-  paths = [ jello_lib hello ];
+
+  nativeBuildInputs = [ pip setuptools hello ];
+
+  buildInputs = [ hello ];
+
+  propagatedBuildInputs = [ hello ];
 }
+
