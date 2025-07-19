2ebdd588e1d89758a5817d8f74d38b18e77a422f
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-19 14:15:29 -0500

    Checkpoint before experimenting with overlay.

--- python.nix
+++ python.nix
@@ -19,4 +19,29 @@ pkgs: pkgs.python313.withPackages (ps: with ps; [
   google-api-python-client
   geoip2
 
+  # This doesn't work
+  # python313Packages.callPackage ./python/callable-module.nix {}
+
+  # This doesn't work
+  # callPackage ./python/callable-module.nix ps
+
+  # This works
+  # (pkgs.callPackage ./python/callable-module.nix ps)
+
+  # This works too
+  # (callPackage ./python/callable-module.nix ps)
+
+  # This works!
+  #(import ./python/callable-module.nix ps)
+
+  # This works
+  (ps.callPackage ./python/callable-module.nix {})
+
+  # This doesn't work, because it needs to depend on
+  # the output of the previous line. How to do that
+  # in the most scalable elegant way that doesn't
+  # require any hacks or kludges that will cause
+  # problems down the road?
+  (ps.callPackage ./python/is-instance.nix {})
+
 ])
