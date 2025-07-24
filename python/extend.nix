11798eb16104862907c466a0c881f0aa2940e4af
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-24 17:05:14 -0500

    There's no way in hell this works

--- python/extend.nix
+++ python/extend.nix
@@ -1,21 +1,25 @@
 # python/extend.nix
 # This is a pythonPackages *scope* extension: self = the extended scope, super = old scope
 
-self: super:
-let
-  call = self.callPackage;
-in {
+{ fetchFromGitHub }:
 
-  callable_module = call ./callable-module.nix {};
-  is_instance     = call ./is-instance.nix { inherit (self) callable_module; };
-  assure          = call ./assure.nix {};
-  mmry            = call ./mmry.nix {};
-  embd            = call ./embd.nix { inherit (self) is_instance assure mmry; };
-  kern            = call ./kern.nix { inherit (self) assure mmry; };
-  wnix            = call ./wnix.nix { inherit (self) assure mmry is_instance embd kern; };
+  self: super:
+  let
+    call = self.callPackage;
+  in {
 
-  jello = call ./jello.nix {
-    inherit (super) lib stdenv fetchFromGitHub;
-    inherit (super.python.pkgs) buildPythonPackage setuptools wheel;
-  };
-}
+    callable_module = call ./callable-module.nix {};
+    is_instance     = call ./is-instance.nix { inherit (self) callable_module; };
+    assure          = call ./assure.nix {};
+    mmry            = call ./mmry.nix {};
+    embd            = call ./embd.nix { inherit (self) is_instance assure mmry; };
+    kern            = call ./kern.nix { inherit (self) assure mmry; };
+    wnix            = call ./wnix.nix { inherit (self) assure mmry is_instance embd kern; };
+
+    jello = call ./jello.nix {
+      inherit fetchFromGitHub;
+      inherit (super) lib stdenv;
+      inherit (self) buildPythonPackage setuptools wheel pip;
+    };
+  }
+
