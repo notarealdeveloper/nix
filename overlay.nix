9e20d1c5cf3f1289637a323bcbf5c6c1265c3f0a
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-24 15:56:55 -0500

    Fix sl hash in overlay

--- overlay.nix
+++ overlay.nix
@@ -5,7 +5,7 @@ final: prev: {
       owner = "mtoyoda";
       repo  = "sl";
       rev   = "923e7d7ebc5c1f009755bdeb789ac25658ccce03";
-      hash  = "173gxk0ymiw94glyjzjizp8bv8g72gwkjhacigd1an09jshdrjb4";
+      sha256  = "173gxk0ymiw94glyjzjizp8bv8g72gwkjhacigd1an09jshdrjb4";
     };
   });
 }
