f331f956e2c1cbabe60f40b5818792978bf26b35
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-25 15:46:28 -0500

    Ok we're either dumb or the world is, this should fix it (?)

--- python/numpy-old-ass.nix
+++ python/numpy-old-ass.nix
@@ -1,10 +1,11 @@
 pkgs: ps:
 
-ps.numpy.overridePythonAttrs (old: rec {
+with ps;
+buildPythonPackage {
+  pname = "numpy";
   version = "1.26.4";
   src = pkgs.fetchPypi {
-    pname = "numpy";
-    inherit version;
+    inherit pname version;
     sha256 = "sha256-KgKrqe0S5KxOs+qUIcQgMBoMZGDZgw10qd+H76SRIBA=";
   };
-})
+}
