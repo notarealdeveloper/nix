b78e8e6c2f25d0c25f1fd1a12dbc313edb7ea553
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-24 21:29:37 -0500

    If this works, I'll be officially in love with Nix forever

--- python/extend.nix
+++ python/extend.nix
@@ -16,7 +16,7 @@ pkgs:
     wnix            = call ./wnix.nix { inherit (self) assure mmry is_instance embd kern; };
 
     lambda-multiprocessing = call ./lambda-multiprocessing.nix { };
-    tflite-runtime = call ./tflite-runtime.nix { };
+    tflite-runtime = call ./tflite-runtime.nix { inherit pkgs; python = pkgs.python3; };
 
     jello = call ./jello.nix {
       inherit (pkgs) lib stdenv fetchFromGitHub;
--- python/tflite-runtime.nix
+++ python/tflite-runtime.nix
@@ -1,50 +1,31 @@
-{ pkgs
-, lib
-, buildPythonPackage
-, fetchPypi
-, pip
-, setuptools
-, wheel
-, python
-, ...
-} @ inputs:
+{ pkgs, python }:
 
-buildPythonPackage rec {
+with python.pkgs;
+buildPythonPackage {
   pname = "tflite-runtime";
   version = "2.14.0";
   format = "wheel";
 
-  # fetch source
-  src = builtins.fetchurl {
-    #inherit pname version;
+  src = pkgs.fetchurl {
     url = "https://files.pythonhosted.org/packages/8f/a6/02d68cb62cd221589a0ff055073251d883936237c9c990e34a1d7cecd06f/tflite_runtime-2.14.0-cp311-cp311-manylinux2014_x86_64.whl";
     sha256 = "195ab752e7e57329a68e54dd3dd5439fad888b9bff1be0f0dc042a3237a90e4d";
   };
 
-  nativeBuildInputs = [
-    pkgs.autoPatchelfHook
-    pkgs.gcc                # for compiling the preload stub
-    pkgs.makeWrapper        # for wrapPythonPrograms if needed
-  ];
+  nativeBuildInputs = [ pkgs.autoPatchelfHook ];
 
   buildInputs = [
-    pkgs.stdenv.cc.cc.lib   # provides libstdc++.so.6
+    pkgs.stdenv.cc.cc.lib
+    pkgs.python311  # only for libpython3.11.so.1.0
   ];
 
   dontWrapPythonPrograms = true;
 
-  # Inject a stub for _PyThreadState_UncheckedGet
   postFixup = ''
-    echo 'void *_PyThreadState_UncheckedGet(void) { return 0; }' > stub.c
-    gcc -shared -fPIC -o $out/lib/fakepy.so stub.c
-
-    # wrap the Python module's loader with LD_PRELOAD
+    echo "🔧 Patching RPATH to include Python 3.11 lib"
     for f in $out/${python.sitePackages}/tflite_runtime/*.so; do
-      echo "Wrapping $f with LD_PRELOAD"
-      mv "$f" "$f.orig"
-      makeWrapper "$f.orig" "$f" \
-        --set LD_PRELOAD "$out/lib/fakepy.so"
+      echo "patching $f"
+      patchelf --set-rpath ${pkgs.python311}/lib:$(patchelf --print-rpath $f) $f
     done
   '';
-
 }
+
