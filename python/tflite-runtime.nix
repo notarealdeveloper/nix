1c3a494a47b439c3f3726f492d4c11d9933d793d
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-25 12:14:07 -0500

    Don't use the ARM wheel lol

--- python/tflite-runtime.nix
+++ python/tflite-runtime.nix
@@ -17,8 +17,8 @@ buildPythonPackage rec {
 
   # fetch source
   src = builtins.fetchurl {
-    url = "https://files.pythonhosted.org/packages/fb/76/e246c39d92929655bac8878d76406d6fb0293c678237e55621e7ece4a269/tflite_runtime-2.14.0-cp311-cp311-manylinux_2_34_armv7l.whl";
-    sha256 = "c4e66a74165b18089c86788400af19fa551768ac782d231a9beae2f6434f7949";
+    url = "https://files.pythonhosted.org/packages/8f/a6/02d68cb62cd221589a0ff055073251d883936237c9c990e34a1d7cecd06f/tflite_runtime-2.14.0-cp311-cp311-manylinux2014_x86_64.whl";
+    sha256 = "195ab752e7e57329a68e54dd3dd5439fad888b9bff1be0f0dc042a3237a90e4d";
   };
 
   build-system = [ setuptools wheel ];
