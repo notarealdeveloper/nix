c6c968b594221ca221a8d86410afcdc3b22a2528
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-25 16:08:56 -0500

    Use fetchurl for the numpy-old-ass wheel

--- python/numpy-old-ass.nix
+++ python/numpy-old-ass.nix
@@ -1,10 +1,11 @@
 pkgs: python:
 
 python.buildPythonPackage {
-  pname = "numpy";
+  name = "numpy";
   version = "1.26.4";
-  src = pkgs.fetchPypi {
-    inherit pname;
+  format = "wheel";
+  src = builtins.fetchurl {
+    url = "https://files.pythonhosted.org/packages/3a/d0/edc009c27b406c4f9cbc79274d6e46d634d139075492ad055e3d68445925/numpy-1.26.4-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
     sha256 = "666dbfb6ec68962c033a450943ded891bed2d54e6755e35e5835d63f4f6931d5";
   };
 }
