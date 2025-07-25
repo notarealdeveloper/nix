7ecb00eb3f4273ba97f967ae04149a396192200e
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-25 15:33:07 -0500

    Toss the 3.11s back into systemPackages as well

--- flake.nix
+++ flake.nix
@@ -49,7 +49,14 @@
           pip
           setuptools
           ipython
-          (numpy.overridePythonAttrs (old: { version = "1.26.4"; }))
+          (ps.numpy.overridePythonAttrs (old: rec {
+            version = "1.26.4";
+            src = pkgs.fetchPypi {
+              pname = "numpy";
+              inherit version;
+              sha256 = "666dbfb6ec68962c033a450943ded891bed2d54e6755e35e5835d63f4f6931d5";
+            };
+          }))
           pandas
           scikit-learn
           lightgbm
--- turing.nix
+++ turing.nix
@@ -239,6 +239,25 @@
     # So close!
     # python-head
 
+    (pkgs.python311.withPackages (ps: with ps; [
+      pip
+      setuptools
+      ipython
+      (ps.numpy.overridePythonAttrs (old: rec {
+        version = "1.26.4";
+        src = pkgs.fetchPypi {
+          pname = "numpy";
+          inherit version;
+          sha256 = "666dbfb6ec68962c033a450943ded891bed2d54e6755e35e5835d63f4f6931d5";
+        };
+      }))
+      pandas
+      scikit-learn
+      lightgbm
+      lambda-multiprocessing
+      tflite-runtime
+    ]))
+
   ];
 
   # Define a user
