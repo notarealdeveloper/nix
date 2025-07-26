df13a738c4d40ebd0cf643a6e8948dc9b53e948b
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-25 19:39:09 -0500

    Move python-head into its own file

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,64 +1,31 @@
 # overlay.nix
 final: prev: rec {
 
+  ############################
+  ### Existing Executables ###
+  ############################
+
   sl2 = import ./bin/sl2.nix final prev;
 
-  python-head = prev.python314.overrideAttrs (old: rec {
-    pname   = "python-head";
-    sourceVersion = {
-      major = "3";
-      minor = "15";
-      patch = "0";
-      suffix = "a1";
-    };
-    version = "${sourceVersion.major}.${sourceVersion.minor}.${sourceVersion.patch}${sourceVersion.suffix}";
-    pythonVersion = "${sourceVersion.major}.${sourceVersion.minor}";
-    src = prev.fetchFromGitHub {
-      owner = "python";
-      repo  = "cpython";
-      rev = "777159fa318f39c36ad60039cdf35a8dbb319637";
-      hash  = "sha256-tXxYVCnwqp11PD5nQaLTHySVv0xf+4wuLN1eC4JiRBw=";
-    };
-
-    configureFlags = (old.configureFlags or []) ++ [
-      "--with-openssl=${prev.openssl.dev}"
-    ];
-
-    # make patchPhase a no-op
-    patches = [];
-    # nixpkgs python also sticks stuff here
-    postPatch = "";
-    patchPhase = ''
-      runHook prePatch
-      # nothing
-      runHook postPatch
-    '';
-
-    doCheck = false;
-
-    #postInstall = "runHook postInstall";
-    postInstall = "";
-
-    propagatedBuildInputs = (old.propagatedBuildInputs or []) ++ [
-      prev.openssl
-    ];
-
-  });
+  python-head = import ./python/python-head.nix final prev; # work in progress
 
   #########################
   ### Local Executables ###
   #########################
 
-  hello = (import ./bin/hello.nix { inherit (prev) stdenv fetchFromGitHub; });
+  hello = import ./bin/hello.nix { inherit (prev) stdenv fetchFromGitHub; };
 
-  yello = (import ./bin/yello.nix prev prev.python3);
+  yello = import ./bin/yello.nix prev prev.python3;
 
   ######################
   ### Python Overlay ###
   ######################
 
+  # Note: Once you have multiple overlays, include them like this
+  # pythonPackagesExtensions = (prev.pythonPackagesExtensions or []) ++ [ (import ./python/extend.nix prev) ];
+
   # 1) Register your extension so *every* pythonXPackages set sees it.
-  pythonPackagesExtensions = (prev.pythonPackagesExtensions or []) ++ [ (import ./python/extend.nix prev) ];
+  pythonPackagesExtensions = [ (import ./python/extend.nix prev ) ];
 
   # 2) (Optional) Expose them at top-level too, like normal pkgs:
   inherit (final.python3Packages) callable_module is_instance assure mmry embd kern wnix jello;
