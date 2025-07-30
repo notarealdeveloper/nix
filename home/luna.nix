9cca629d36885091cddd74bd6591c4bee85c073a
jason wilkes <notarealdeveloper@gmail.com>
2025-07-30 13:39:40 -0500

    Update home/luna.nix

--- home/luna.nix
+++ home/luna.nix
@@ -6,14 +6,37 @@
   home.homeDirectory = "/home/luna";
 
   home.packages = with pkgs; [
-    cowsay
-    xcowsay
-    figlet
-    toilet
-    cmatrix
-    asciiquarium
     yt-dlp
+    vlc
   ];
+
+  # git clone repos if not present
+  home.activation.prepareDaXingXing = lib.hm.dag.entryAfter ["writeBoundary" "installPackages"] ''
+    export PATH="${pkgs.yt-dlp}/bin:$PATH"
+
+    mkdir -pv "${config.home.homeDirectory}/bin"
+    mkdir -pv "${config.home.homeDirectory}/daxingxing"
+    chmod +x ${config.home.homeDirectory}/bin/kan
+
+    yt-dlp -P "$HOME/daxingxing" \
+      https://www.youtube.com/watch?v=06Kg6XeMhQU \
+      https://www.youtube.com/watch?v=ZZiTNJ6QleA \
+      https://www.youtube.com/watch?v=B1u-ylQR6Fo
+  ''
+
+  home.file."bin/kan".text = ''
+    #!/bin/sh
+    export PATH="${pkgs.vlc}/bin:$PATH"
+    exec vlc "$HOME/daxingxing"
+  '';
+
+  # ensure ~/bin/kan is executable
+  home.activation.chmodKan = lib.hm.dag.entryAfter ["linkGeneration"] ''
+    chmod +x "$HOME/bin/kan"
+  '';
+
+  # add ~/bin to PATH persistently
+  home.sessionPath = [ "$HOME/bin" ];
 
   # Keep this line
   home.stateVersion  = "25.05";
