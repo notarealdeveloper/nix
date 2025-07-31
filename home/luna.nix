191e0f2465d37eaa1c2570cdbd46e86ec868f1f6
Jason Wilkes <notarealdeveloper@gmail.com>
2025-07-31 14:39:17 -0500

    Optimize re-kan logic and add kans to home/luna.nix

--- home/luna.nix
+++ home/luna.nix
@@ -28,19 +28,34 @@ in
     mkdir -pv "${config.home.homeDirectory}/daxingxing"
 
     mkkan() {
-      yt-dlp -P "$HOME/daxingxing" -o "$1.%(ext)s" "$2"
+      local name="$1"
+      local url="$2"
+      if [[ $(ls "$HOME/daxingxing/$name"* | wc -l) == 0 ]]; then
+        echo "Making kan: ${1}"
+        yt-dlp -P "$HOME/daxingxing" -o "$name.%(ext)s" "$url"
+      else
+        echo "Already have kan: ${1}"
+      fi
     }
 
-    mkkan yayomyca   https://www.youtube.com/watch?v=ZZiTNJ6QleA
-    mkkan cawoomwoom https://www.youtube.com/watch?v=B1u-ylQR6Fo
-    mkkan driving    https://www.youtube.com/watch?v=BdrZWu2dZ4c
-    mkkan eieio      https://www.youtube.com/watch?v=_6HzoUcx3eo
-    mkkan happy      https://www.youtube.com/watch?v=06Kg6XeMhQU
-    mkkan babyshark  https://www.youtube.com/watch?v=XqZsoesa55w
-    mkkan putshoes   https://www.youtube.com/watch?v=D_FGBpQ0iOg
-    mkkan sunnyday   https://www.youtube.com/watch?v=CEFL-4weVsI
-    mkkan abcquack   https://www.youtube.com/watch?v=I_3mbra4dHU
-    mkkan monster    https://www.youtube.com/watch?v=JC29ZvTkBT0
+    mkkan yayomyca      https://www.youtube.com/watch?v=ZZiTNJ6QleA
+    mkkan cawoomwoom    https://www.youtube.com/watch?v=B1u-ylQR6Fo
+    mkkan driving       https://www.youtube.com/watch?v=BdrZWu2dZ4c
+    mkkan eieio         https://www.youtube.com/watch?v=_6HzoUcx3eo
+    mkkan happy         https://www.youtube.com/watch?v=06Kg6XeMhQU
+    mkkan babyshark     https://www.youtube.com/watch?v=XqZsoesa55w
+    mkkan putshoes      https://www.youtube.com/watch?v=D_FGBpQ0iOg
+    mkkan sunnyday      https://www.youtube.com/watch?v=CEFL-4weVsI
+    mkkan abcquack      https://www.youtube.com/watch?v=I_3mbra4dHU
+    mkkan monster       https://www.youtube.com/watch?v=JC29ZvTkBT0
+    mkkan ifyoulike     https://www.youtube.com/watch?v=UXt2bNp77u0
+    mkkan yayomyca2     https://www.youtube.com/watch?v=zXwZ28L6v7U
+    mkkan teninthebed   https://www.youtube.com/watch?v=fu_xY9kMSBU
+    mkkan wheelsonbus   https://www.youtube.com/watch?v=AvJAzwCGQKw
+    mkkan crawllikea    https://www.youtube.com/watch?v=PoZdClg4SQc
+    mkkan whocookie     https://www.youtube.com/watch?v=Vu0-98fgnRo
+    mkkan areyouhungry  https://www.youtube.com/watch?v=vACFjPUFHM4
+    mkkan thisisahappy  https://www.youtube.com/watch?v=W9rX6ApYqjo
   '';
 
   home.file."bin/kan" = {
