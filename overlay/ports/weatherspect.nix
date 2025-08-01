1f52ec159cebe1ddf9360a8a33d3061e60ca2249
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-01 15:37:46 -0500

    HAPPIER, AND WITH YOUR MOUTH OPEN !

--- overlay/ports/weatherspect.nix
+++ overlay/ports/weatherspect.nix
@@ -1,35 +1,44 @@
 # weatherspect.nix
-pkgs: with pkgs;
+pkgs: (with pkgs;
 
-stdenv.mkDerivation rec {
+  stdenv.mkDerivation rec {
 
-  pname = "weatherspect";
-  version = "2.0";
+    pname = "weatherspect";
+    version = "2.0";
 
-  src = fetchFromGitHub {
-    owner = "AnotherFoxGuy";
-    repo  = "weatherspect";
-    rev   = version;
-    hash  = "sha256-rTZTNcnehL6PR5VagCMTs1MnRCap/XXjxQMSZyBMiuE=";
-  };
+    src = fetchFromGitHub {
+      owner = "AnotherFoxGuy";
+      repo  = "weatherspect";
+      rev   = version;
+      hash  = "sha256-rTZTNcnehL6PR5VagCMTs1MnRCap/XXjxQMSZyBMiuE=";
+    };
 
-  nativeBuildInputs = [
-    (perl.withPackages (ps: with ps; [
-      TermAnimation
-    ]))
-  ];
+    nativeBuildInputs = [
+      (perl.withPackages (ps: with ps; [
+        TermAnimation
+      ]))
+    ];
 
-  installPhase = ''
-    mkdir -pv $out/bin
-    mv -v * $out/bin    # b/c idk what's in this shit, I'm just here
-  '';
+    installPhase = ''
+      mkdir -pv $out/bin
 
-  meta = with lib; {
-    description = "I'm trying to get ascii weather";
-    homepage    = "They all died long ago";
-    license     = licenses.wtfpl;
-    platforms   = platforms.unix;
-  };
+      echo "let's see what we've got"
+      ls
 
-}
+      # cuz fuck it, that's why
+      chmod -v +x *
 
+      # b/c idk what's in this shit, I'm just here
+      mv -v * $out/bin
+    '';
+
+    meta = with lib; {
+      description = "I'm trying to get ascii weather";
+      homepage    = "They all died long ago";
+      license     = licenses.wtfpl;
+      platforms   = platforms.unix;
+    };
+
+  }
+
+)
