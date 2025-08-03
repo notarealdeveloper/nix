a4b987a8d1e69b04a599c7dc34aeefab8650cf0e
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-03 11:14:54 -0500

    Add overlay/python/lightgbm.nix from nixpkgs and depend on numpy_1

--- overlay/python/extend.nix
+++ overlay/python/extend.nix
@@ -23,5 +23,6 @@ pkgs:
     lambda-multiprocessing  = call ./lambda-multiprocessing.nix { };
     tflite-runtime          = call ./tflite-runtime.nix { inherit pkgs; python = pkgs.python3; };
     dvc-s3                  = call ./dvc-s3.nix { inherit pkgs; python = pkgs.python3; };
+    lightgbm                = call ./lightgbm.nix { inherit pkgs; python = pkgs.python3; };
 
   }
