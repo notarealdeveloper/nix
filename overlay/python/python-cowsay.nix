30d6a360620925540455edd973400fcdaaba1c54
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-07 18:39:22 -0500

    Add python-cowsay to overlay

---
+++ overlay/python/python-cowsay.nix
@@ -0,0 +1,20 @@
+
+{ lib
+, buildPythonPackage
+, fetchPypi
+, python-cowsay
+, ... }:
+
+buildPythonPackage rec {
+  pname = "python-cowsay";
+  version = "1.2.1";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "e4d6ff9794cbf776129ffbef9addcda0021ad4e63b6cd8cbe9c12ea415311139";
+  };
+
+  propagatedBuildInputs = [
+  ];
+}
