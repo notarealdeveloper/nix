994d4f3cdd304d2a38cd620c477c249778ebb79c
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-24 10:34:39 -0500

    GETTING CLOSER

--- python/default.nix
+++ python/default.nix
@@ -189,11 +189,25 @@ let
 
         # First run will print a "wanted" hash; paste it into both of these.
         vendorHash = final.lib.fakeHash;
-        fetchAttrs = {
-          hash = final.lib.fakeHash;
-          dontConfigure = true;  # avoid running ./configure on vendored repos
-        };
 
+        fetchAttrs = {
+          # FOD fields
+          outputHashMode = "recursive";
+          outputHashAlgo = "sha256";
+          outputHash = "sha256-SKSfciHCDU4qZ2GvT9Zrs0Rfd8zF10SNkiLoSBbsh5I=";
+
+          # kill default phases that poke at configure.py / external outputs
+          dontConfigure = true;
+          dontBuild = true;
+
+          # produce a trivial tarball so $out exists; Nix will immediately
+          # tell you the correct hash to paste back into outputHash.
+          installPhase = ''
+            # create an empty tar.gz at $out deterministically
+            mkdir -p "$TMPDIR/empty"
+            tar -czf "$out" -C "$TMPDIR" empty --owner=0 --group=0 --mtime=@0
+          '';
+        };
         nativeBuildInputs = [
           final.llvmPackages.clang
           pyfinal.wheel
