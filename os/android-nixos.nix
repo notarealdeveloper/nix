5f74de797591979f905d6a42c38f5506cee96275
jason <notarealdeveloper@gmail.com>
2025-08-25 15:28:44 -0500

    Update nix for android

--- os/android-nixos.nix
+++ os/android-nixos.nix
@@ -31,20 +31,26 @@
     asciiquarium
     sl
     python313
+    passwd
     home-manager
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
   environment.etcBackupExtension = ".bak";
 
-  # Read the changelog before changing this value
-  system.stateVersion = "24.05";
-
   # Set up nix for flakes
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
 
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = true;
+  services.openssh.settings.PermitRootLogin = "no";
+
   # Set your time zone
   time.timeZone = "America/Chicago";
+
+  # Don't change this
+  system.stateVersion = "24.05";
+
 }
