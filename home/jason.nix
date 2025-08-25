47d32b3f360875d612b8db9951c66d76b1a4d8b3
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-25 12:50:04 -0500

    C'mon man

--- home/jason.nix
+++ home/jason.nix
@@ -15,28 +15,21 @@ in {
   home.homeDirectory = "/home/${user}";
   news.display = "silent";
 
-  programs = {
-
-    # ~/.config/git
-    git = {
-      enable    = true;
-      userName  = name;
-      userEmail = email;
-      extraConfig = {
-        init.defaultBranch = "master";
-        pull.rebase = true;
-      };
+  # ~/.config/git
+  programs.git = {
+    enable    = true;
+    userName  = name;
+    userEmail = email;
+    extraConfig = {
+      init.defaultBranch = "master";
+      pull.rebase = true;
     };
+  };
 
-    # ~/.config/gh
-    gh = {
-      enable = true;
-      gitCredentialHelper.enable = true;
-    };
-
-    bash = {
-      enable = true;
-    };
+  # ~/.config/gh
+  programs.gh = {
+    enable = true;
+    gitCredentialHelper.enable = true;
   };
 
 
