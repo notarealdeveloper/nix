3eab96d1ff37d1a76d1491785ce4e0febd2c1220
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-25 12:52:08 -0500

    Simplify home

--- home/jason.nix
+++ home/jason.nix
@@ -7,7 +7,10 @@ let
   email = "notarealdeveloper@gmail.com";
 
   src = import ./src.nix;
-  inherit (src) personal;
+  inherit (src) nix exec personal;
+  inherit (src) secret legacy;
+
+  link = config.lib.file.mkOutOfStoreSymlink;
 
 in {
 
@@ -84,6 +87,120 @@ in {
 
   };
 
+  # git clone public repos
+  home.activation.clonePublic = lib.hm.dag.entryAfter ["writeBoundary" "installPackages"] ''
+
+    export PATH="${pkgs.git}/bin:$PATH"
+
+    if [ ! -d "${nix.dst}" ]; then
+      git clone "${nix.src}" "${nix.dst}"
+    fi
+
+    if [ ! -d "${exec.dst}" ]; then
+      git clone "${exec.src}" "${exec.dst}"
+    fi
+
+    if [ ! -d "${personal.dst}" ]; then
+      git clone "${personal.src}" "${personal.dst}"
+    fi
+  '';
+
+  home.file = lib.mkMerge [{
+
+    # symlinks from exec
+    ".vimrc".source     = link "${exec.dst}/etc/vimrc";
+    ".face".source      = link "${exec.dst}/etc/dot-face";
+
+    # ipython startup file
+    ".ipython/profile_default/startup/00-pyrc.py".source = link "${exec.dst}/bin/pyrc";
+
+    # symlinks from personal
+    "Templates".source  = link "${personal.dst}/etc/Templates";
+
+    # auto-generated
+    ".hotdogrc".text    = ''This is not a config file'';
+
+  }];
+
+  # PATH for interactive shells
+  # home.sessionVariables.PATH = lib.mkBefore "${exec.dst}/bin:${personal.dst}/bin";
+
+
+  # PATH for login shells
+  home.sessionPath = [
+    "${exec.dst}/bin"
+    "${personal.dst}/bin"
+  ];
+
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+      source "${exec.dst}/etc/bashrc"
+      source "${personal.dst}/etc/bashrc"
+    '';
+  };
+
+  home.packages = with pkgs; [
+    git
+    dconf
+    inotify-tools
+  ];
+
+  xdg.configFile = {
+
+    # ~/.config/autostart/conky.desktop
+    "autostart/conky.desktop" = {
+      text = ''
+        [Desktop Entry]
+        Type=Application
+        Name=Conky
+        Exec=${personal.dst}/conky-smart-start
+        X-GNOME-Autostart-enabled=true
+        NoDisplay=false
+        Comment=Start Conky at login
+      '';
+    };
+  };
+
+  # git clone private repos
+  home.activation.clonePrivate = lib.hm.dag.entryAfter ["writeBoundary" "installPackages"] ''
+
+    export PATH="${pkgs.git}/bin:$PATH"
+
+    if [ ! -d "${secret.dst}" ]; then
+      git clone "${secret.src}" "${secret.dst}"
+    fi
+
+    if [ ! -d "${legacy.dst}" ]; then
+      git clone "${legacy.src}" "${legacy.dst}"
+    fi
+
+  '';
+
+  home.file = lib.mkMerge [{
+    # sooper serious secrets zomg
+    ".pypirc".source    = link "${secret.dst}/etc/pypirc";
+    ".netrc".source     = link "${secret.dst}/etc/netrc";
+    ".ssh".source       = link "${secret.dst}/etc/ssh";
+
+    # gitlab
+    ".config/glab-cli/config.yml".source = link "${secret.dst}/etc/glab-cli-config.yml";
+  }];
+
+  # PATH for interactive shells
+  # home.sessionVariables.PATH = lib.mkBefore "${secret.dst}/bin";
+
+  # PATH for login shells
+  home.sessionPath = [
+    "${secret.dst}/bin"
+  ];
+
+  programs.bash = {
+    enable = true;
+    bashrcExtra = lib.mkAfter ''
+      source "${secret.dst}/etc/bashrc"
+    '';
+  };
   # Let Home Manager install and manage itself.
   # programs.home-manager.enable = true;
 
