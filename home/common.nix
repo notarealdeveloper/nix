e4486937d69c4d3b13d7612a7b93e7acf85b9876
Jason Wilkes <notarealdeveloper@gmail.com>
2025-08-25 09:07:12 -0500

    This?

--- home/common.nix
+++ home/common.nix
@@ -1,4 +1,4 @@
-user:
+{ lib, config, user, ... }:
 
 {
   home.username = "${user}";
--- home/jason.nix
+++ home/jason.nix
@@ -1,29 +1,7 @@
-{ pkgs, lib, config, desktop ? true, ... }:
+{ lib, config, user, ... }:
 
-let
-
-  user = "jason";
-  name = "Jason Wilkes";
-  email = "notarealdeveloper@gmail.com";
-  common    = (import ./common.nix user);
-  gitconfig = (import ./lib/git.nix name email);
-
-  system = import ./lib/system.nix { inherit pkgs lib config; };
-  inherit (system) nixos exec personal;
-
-in {
-
-  imports = [
-    common
-    gitconfig
-    ./lib/public.nix
-    ./lib/private.nix
-    (if desktop then ./lib/desktop.nix else ./lib/none.nix)
-  ];
-
-  # Let Home Manager install and manage itself.
-  # programs.home-manager.enable = true;
-
-  # Keep this line
-  home.stateVersion  = "25.05";
+{
+  home.username = "${user}";
+  home.homeDirectory = "/home/${user}";
+  news.display = "silent";
 }
