cd2a1ecdf8107563025f640148d0d422f2dc92de
jason <notarealdeveloper@gmail.com>
2025-08-26 14:29:42 -0500

    Fix obsidian icon by re-adding home dir

--- home/jason.nix
+++ home/jason.nix
@@ -143,7 +143,7 @@ in {
       ".local/share/applications/obsidian.desktop".text =
           let
             src = builtins.readFile "${pkgs.obsidian}/share/applications/obsidian.desktop";
-            icon = ".local/share/icons/Numix-Circle/scalable/apps/obsidian.png";
+            icon = "${config.home.homeDirectory}.local/share/icons/Numix-Circle/scalable/apps/obsidian.png";
           in
             builtins.replaceStrings ["Icon=obsidian"] ["Icon=${icon}"] src;
     })
