478f6ace6b9365e570b542018fc2aa48c9fbe2de
Jason Wilkes <notarealdeveloper@gmail.com>
2025-09-25 10:06:18 -0500

    Add some languages for the book.

--- configuration.nix
+++ configuration.nix
@@ -299,9 +299,14 @@ in
     noelf
 
     # for the book
-    pango
     bc
-    rustc
+    pango
+    gfortran  # Fortran. (1957). John Backus and his team at IBM.
+    gnucobol  # Cobol. (1959). Designed by the CODASYL committee, partly based on Grace Hopper's FLOW-MATIC language.
+    clisp     # Lisp. (1956 to 1960). John McCarthy developed the basics during a summer AI research project.
+    guile     # Scheme. (1975). Guy Steele and Gerald Sussman, the "Lambda the Ultimate" papers.
+    rustc     # Rust. (2006 to 2015). Graydon Hoare created Rust in 2006, Mozilla sponsored it in 2009. Released 2012. Stable 2015.
+
 
   ] ++ wnixpkgs;
 
