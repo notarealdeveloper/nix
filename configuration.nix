6850a22e176cf16178ed3c839745017763500e5a
Jason Wilkes <notarealdeveloper@gmail.com>
2025-09-25 19:47:46 -0500

    Add poppler-utils

--- configuration.nix
+++ configuration.nix
@@ -203,6 +203,7 @@ in
     dropbox
     gparted
     obsidian
+    poppler-utils
     google-chrome
     gnome-terminal
     github-desktop
@@ -306,7 +307,6 @@ in
     clisp     # Lisp. (1956 to 1960). John McCarthy developed the basics during a summer AI research project.
     guile     # Scheme. (1975). Guy Steele and Gerald Sussman, the "Lambda the Ultimate" papers.
     rustc     # Rust. (2006 to 2015). Graydon Hoare created Rust in 2006, Mozilla sponsored it in 2009. Released 2012. Stable 2015.
-
 
   ] ++ wnixpkgs;
 
