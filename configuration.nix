455ca3cde21a8fe030e1d9513e0f43996f2b4022
Jason Wilkes <root@thedynamiclinker.com>
2026-06-29 12:23:43 -0500

    Try to fix the build.

--- configuration.nix
+++ configuration.nix
@@ -15,14 +15,9 @@ let
   cacheName = "notarealdeveloper";
 
   numix-gtk-theme-fixed = pkgs.numix-gtk-theme.overrideAttrs (old: {
-    postBuild = (old.postBuild or "") + ''
-      substituteInPlace src/gtk-3.20/dist/gtk.css \
+    postPatch = (old.postPatch or "") + ''
+      substituteInPlace src/gtk-3.20/scss/apps/_thunar.scss \
         --replace-fail "border-top-width: 1;" "border-top-width: 1px;"
-
-      substituteInPlace src/gtk-3.20/dist/gtk-dark.css \
-        --replace-warn "border-top-width: 1;" "border-top-width: 1px;"
-
-      glib-compile-resources --sourcedir=src/gtk-3.20 src/gtk-3.20/gtk.gresource.xml
     '';
   });
 
--- home/jason.nix
+++ home/jason.nix
@@ -22,14 +22,9 @@ let
   link = config.lib.file.mkOutOfStoreSymlink;
 
   numix-gtk-theme-fixed = pkgs.numix-gtk-theme.overrideAttrs (old: {
-    postBuild = (old.postBuild or "") + ''
-      substituteInPlace src/gtk-3.20/dist/gtk.css \
+    postPatch = (old.postPatch or "") + ''
+      substituteInPlace src/gtk-3.20/scss/apps/_thunar.scss \
         --replace-fail "border-top-width: 1;" "border-top-width: 1px;"
-
-      substituteInPlace src/gtk-3.20/dist/gtk-dark.css \
-        --replace-warn "border-top-width: 1;" "border-top-width: 1px;"
-
-      glib-compile-resources --sourcedir=src/gtk-3.20 src/gtk-3.20/gtk.gresource.xml
     '';
   });
 
