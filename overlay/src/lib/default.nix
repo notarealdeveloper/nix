476a420242fc370b76d1bb1557db7dcf84409091
Jason Wilkes <root@thedynamiclinker.com>
2026-07-17 20:09:51 -0500

    Revert "Wut"
    This reverts commit 2cea79220eef1a4cb0f8666600f50e7b132b4bcc.

--- overlay/src/lib/default.nix
+++ overlay/src/lib/default.nix
@@ -1,16 +1,3 @@
 final: prev:
 
   (import ./python final prev)
-  //
-  (let
-    unsupportedHwlocFlags = [
-      "--enable-netloc"
-    ];
-  in {
-    hwloc = prev.hwloc.overrideAttrs (old: {
-      configureFlags =
-        builtins.filter
-          (flag: !(builtins.elem flag unsupportedHwlocFlags))
-          (old.configureFlags or []);
-    });
-  })
